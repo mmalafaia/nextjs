bbdb36a05b09646ee4df24887013a434d563b2b6
Manoel Malafaia <mmalafaia@gmail.com>
2020-01-03 16:59:24 -0300

    Next completed

---
+++ pages/users.js
@@ -0,0 +1,39 @@
+import React from "react";
+import axios from "axios";
+
+import Link from "next/link";
+import Head from "next/head";
+
+import withAnalytics from "../src/hocs/withAnalytics";
+
+const Users = ({ users }) => (
+  <div>
+    <Head>
+      <title>Usuários</title>
+    </Head>
+    <ul>
+      {users.map(user => (
+        <li key={user.id}>
+          {user.login}
+          <Link href={`/users/${user.login}`}>
+            <a>Acessar Perfil</a>
+          </Link>
+        </li>
+      ))}
+    </ul>
+    <Link href="/">
+      <a>Voltar </a>
+    </Link>
+  </div>
+);
+
+Users.getInitialProps = async () => {
+  const response = await axios.get(
+    "https://api.github.com/orgs/rocketseat/members"
+  );
+  console.log(response.data);
+
+  return { users: response.data };
+};
+
+export default withAnalytics()(Users);
